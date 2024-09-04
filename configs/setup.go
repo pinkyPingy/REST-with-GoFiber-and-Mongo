4dcba8c722d08a92b2fe8cd3bd3d48894ca7b261
Awat Singdamrong <a.lubing@yahoo.com>
2024-09-04 22:37:01 +0700

    fix: Database name

--- configs/setup.go
+++ configs/setup.go
@@ -36,6 +36,6 @@ var DB *mongo.Client = ConnectDB()
 
 // getting database collections
 func GetCollection(client *mongo.Client, collectionName string) *mongo.Collection {
-	collection := client.Database("event").Collection(collectionName)
+	collection := client.Database("Meet-and-Feat").Collection(collectionName)
 	return collection
 }
